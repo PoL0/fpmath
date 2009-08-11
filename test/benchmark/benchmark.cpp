d1d8727817efa310dc41ca1afe266c4a76b15fdc
ps <ps@cf1103ee-5a6b-11de-b96a-e1c1f561d0d2>
2009-08-11 08:30:43 +0000

    Porting to gcc.

--- test/benchmark/benchmark.cpp
+++ test/benchmark/benchmark.cpp
@@ -79,49 +79,6 @@ inline unsigned int cpu_clocks_32()
 }
 
 
-/******************************************************************************/
-/*                                                                            */
-/* cpu_clocks_64                                                              */
-/*                                                                            */
-/******************************************************************************/
-
-/// Return the number of clocks since the processor was powered up.
-//!
-//! With modern processors there are a few gotchas related to this function.
-//!
-//! The function internally uses the RDTSC assembler instruction, which has the
-//! following limitations you should know about:
-//! - The timing information is locked to the cycle counter of the processor.
-//! - Dual core or multi core processors do not guarantee synchronization
-//! between cycle counters.
-//! - CPU frequency is not fixed.
-//!
-//! Nevertheless, the function is a good measurement tool if you want to judge
-//! the relative performance of an algorithm in some measurement such as
-//! clocks per pixel.
-//!
-//! \return The number of clocks since the processor was powered up. The 
-//! result is returned in a 64 bit unsigned integer. It can count up to 
-//! 18.4 * 10^15 counts, which - on a 3 MHz CPU - translates to 585 * 10^6
-//! years before it wraps.
-//!
-//! Wrap around may lead to incorrect measurements since the difference between
-//! a counter value and a wrapped around counter value is meaningless. With the
-//! 64bit counter wrap around with current CPU speeds is virtually impossible.
-inline unsigned __int64 cpu_clocks_64()
-{
-	// The RDTSC statement returns the number of clocks since the processor was
-	// powered up in eax/edx. The intrinsic is used, because the 64bit compiler
-	// does not support inline assembly, like:
-	//	_asm {
-	//		RDTSC
-	//	}
-	// the return statement can be omitted, since the data is already in eax
-
-	return __rdtsc();
-}
-
-
 int main()
 {
 	std::cout << "Benchmark" << std::endl;
@@ -135,143 +92,143 @@ int main()
 	float af = 1.0;
 	float bf = 2.0;
 	float cf;
-	unsigned __int64 start = cpu_clocks_64();
+	unsigned __int64 start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cf = af+bf;
 	}
-	double clocks_per_addition_float = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_addition_float = (cpu_clocks_32() - start) / (double)loops;
 
 	double ad = 1.0;
 	double bd = 2.0;
 	double cd;
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cd = ad+bd;
 	}
-	double clocks_per_addition_double = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_addition_double = (cpu_clocks_32() - start) / (double)loops;
 
 	fpml::fixed_point<int, 15> afp = 1.0;
 	fpml::fixed_point<int, 15> bfp = 2.0;
 	fpml::fixed_point<int, 15> cfp;
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cfp = afp+bfp;
 	}
-	double clocks_per_addition_fp15_16 = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_addition_fp15_16 = (cpu_clocks_32() - start) / (double)loops;
 
 	std::cout << "Addition\t" << clocks_per_addition_float << "\t" << clocks_per_addition_double << "\t" << clocks_per_addition_fp15_16 << std::endl;
 
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cf = af*bf;
 	}
-	double clocks_per_multiplication_float = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_multiplication_float = (cpu_clocks_32() - start) / (double)loops;
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cd = ad*bd;
 	}
-	double clocks_per_multiplication_double = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_multiplication_double = (cpu_clocks_32() - start) / (double)loops;
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cfp = afp*bfp;
 	}
-	double clocks_per_multiplication_fp15_16 = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_multiplication_fp15_16 = (cpu_clocks_32() - start) / (double)loops;
 
 	std::cout << "Multiplication\t" << clocks_per_multiplication_float << "\t" << clocks_per_multiplication_double << "\t" << clocks_per_multiplication_fp15_16 << std::endl;
 
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cf = af/bf;
 	}
-	double clocks_per_division_float = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_division_float = (cpu_clocks_32() - start) / (double)loops;
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cd = ad/bd;
 	}
-	double clocks_per_division_double = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_division_double = (cpu_clocks_32() - start) / (double)loops;
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cfp = afp/bfp;
 	}
-	double clocks_per_division_fp15_16 = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_division_fp15_16 = (cpu_clocks_32() - start) / (double)loops;
 
 	std::cout << "Division\t" << clocks_per_division_float << "\t" << clocks_per_division_double << "\t" << clocks_per_division_fp15_16 << std::endl;
 
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cf = sqrt(af);
 	}
-	double clocks_per_sqrt_float = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_sqrt_float = (cpu_clocks_32() - start) / (double)loops;
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cd = sqrt(ad);
 	}
-	double clocks_per_sqrt_double = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_sqrt_double = (cpu_clocks_32() - start) / (double)loops;
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cfp = sqrt(afp);
 	}
-	double clocks_per_sqrt_fp15_16 = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_sqrt_fp15_16 = (cpu_clocks_32() - start) / (double)loops;
 
 	std::cout << "Sqrt\t" << clocks_per_sqrt_float << "\t" << clocks_per_sqrt_double << "\t" << clocks_per_sqrt_fp15_16 << std::endl;
 
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cf = sin(af);
 	}
-	double clocks_per_sine_float = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_sine_float = (cpu_clocks_32() - start) / (double)loops;
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cd = sin(ad);
 	}
-	double clocks_per_sine_double = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_sine_double = (cpu_clocks_32() - start) / (double)loops;
 
-	start = cpu_clocks_64();
+	start = cpu_clocks_32();
 	for (int i=0; i<loops; ++i)
 	{
 		g = 0;
 		cfp = sin(afp);
 	}
-	double clocks_per_sine_fp15_16 = (cpu_clocks_64() - start) / (double)loops;
+	double clocks_per_sine_fp15_16 = (cpu_clocks_32() - start) / (double)loops;
 
 	std::cout << "Sine\t" << clocks_per_sine_float << "\t" << clocks_per_sine_double << "\t" << clocks_per_sine_fp15_16 << std::endl;
 }
